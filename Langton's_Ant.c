241a51519009d6ceff524a9156f8bb95ea3a014e
edi3d3d <edi3d3d@gmail.com>
2025-03-09 04:58:19 +0200

    changed the print function, instead of clearing the screen and then reprinting the whole thing, now i just move the cursor to 0,0 and reprint over the already existing terminal data

--- Langton's_Ant.c
+++ Langton's_Ant.c
@@ -9,10 +9,10 @@
 // if its 0 the first part will be 0+' ' = ' '
 // if it 1 the first part will be 'O' - ' ' + ' ' = 'O'
 // now i can use int8 
-#define screen_multiplier 1                  //7     multiplier for bigger/smaller dimensions
-#define width  (150*screen_multiplier)       //208   width 10px                             100
-#define height (150*screen_multiplier)        //75    height 24px per h=2.4w                 42 //one small screen square
-#define steps 10                              //nr of steps to do each simulation before displaying it 
+#define width  (800)       //800
+#define height (500)       //500
+#define steps 1         //nr of steps to do each simulation before displaying it 
+#define final 0
 #define R 3
 #define L 1
 void get_input(int *fps)       //receives input from the keyboard for adding cells, changing fps, it also pauses and stops the simulation
@@ -58,65 +58,6 @@ void get_input(int *fps)       //receives input from the keyboard for adding cel
         }
     } while ((key==' ' && pause == 1) || step == 1 || *fps==0);
 }
-void update_ant(char cells[][width], int *ant_x, int *ant_y, int *ant_direction, int *gen, int time_start, int *current_time, int *fps)
-{
-    clock_t start=clock(), stop;
-
-    for(int k=1;k<=steps;k++)
-    {
-        int ant_state, nr_states=2, turn;
-
-        char states[12]        ={' ','0','1','2','3','4','5','6','7','8','9','@'};
-        int states_rotation[12]={ R , L , R , R , R , L , R , L , R , L , L , R };      //CHANGE THE NUMBERS INTO R OR L FOR THAT RESPECTIVE STEP TO TURN RIGHT OR LEFT
-
-        for(int i=0; i<10; i++)
-            if(cells[*ant_y][*ant_x] == states[i]) ant_state=i;
-        
-        turn=states_rotation[ant_state];
-
-        *ant_direction=(*ant_direction+turn)%4;                 //rotate ant
-        
-
-        
-
-        cells[*ant_y][*ant_x]=states[(ant_state+1)%nr_states];  
-
-        
-        if(*ant_direction==0) (*ant_x)+=1;                  //move ant
-        else if(*ant_direction==1) (*ant_y)-=1;
-        else if(*ant_direction==2) (*ant_x)-=1;
-        else if(*ant_direction==3) (*ant_y)+=1;
-        printf("Direction: %d, Position: (%d, %d)\n", *ant_direction, *ant_x, *ant_y);
-
-        
-
-        if(*ant_x>=width)                                   //check out of bounds
-        {
-            printf("Ant out of bounds (right)\n");
-            exit(1);
-        } else if(*ant_y<0)
-        {
-            printf("Ant out of bounds (up)\n");
-            exit(1);
-        } else if(*ant_x<0)
-        {
-            printf("Ant out of bounds (left)\n");
-            exit(1);
-        } else if(*ant_y>=height)
-        {
-            printf("Ant out of bounds (down)\n");
-            exit(1);
-        }
-        
-        *gen+=1;
-    }
-    stop=clock();
-    *current_time=stop-time_start;
-    
-    int sleep_time;
-    if(*fps!=0) sleep_time = 1000 / (*fps) - (stop - start);
-    if (sleep_time > 0 && *fps!=0) Sleep(sleep_time);              //sleep for some time so the fps is more fluid and it doesnt run to quickly in the beginning
-}
 void print_cells(char cells[][width], int ant_x, int ant_y, int gen, int current_time, int fps)
 {
     char printed_cells[((width+2)*(height+3))]={};
@@ -134,10 +75,74 @@ void print_cells(char cells[][width], int ant_x, int ant_y, int gen, int current
     printed_cells[k++]='\0';
 
     //system("cls");
+    COORD coord = {0, 0};
+
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 
     puts(printed_cells);
     
     printf("Gen: %d    Time: %d    Max Fps: %d\nPress q to exit, k to step one frame, space to pause/resume and f to change the fps", gen, current_time/1000, fps);
+    
+}
+void update_ant(char cells[][width], int *ant_x, int *ant_y, int *ant_direction, int *gen, int time_start, int *current_time, int *fps)
+{
+    clock_t start=clock(), stop;
+
+    for(int k=1;k<=steps;k++)
+    {
+        int ant_state, nr_states=7, turn;
+
+        char states[12]        ={' ','0','1','2','3','4','5','6','7','8','9','@'};
+        int states_rotation[12]={ R , R , L , L , L , R , L , L , L , R , R , R };      //CHANGE THE NUMBERS INTO R OR L FOR THAT RESPECTIVE STEP TO TURN RIGHT OR LEFT
+
+        for(int i=0; i<10; i++)
+            if(cells[*ant_y][*ant_x] == states[i]) ant_state=i;
+        
+        turn=states_rotation[ant_state];
+
+        *ant_direction=(*ant_direction+turn)%4;                 //rotate ant
+
+        cells[*ant_y][*ant_x]=states[(ant_state+1)%nr_states];  
+
+        
+        if(*ant_direction==0) (*ant_x)+=1;                  //move ant
+        else if(*ant_direction==1) (*ant_y)-=1;
+        else if(*ant_direction==2) (*ant_x)-=1;
+        else if(*ant_direction==3) (*ant_y)+=1;
+        //printf("Direction: %d, Position: (%d, %d)\n", *ant_direction, *ant_x, *ant_y);
+
+        
+
+        if(*ant_x>=width)                                   //check out of bounds
+        {
+            print_cells(cells, *ant_x, *ant_y, *gen, *current_time, *fps);
+            printf("Ant out of bounds (right)\n");
+            exit(1);
+        } else if(*ant_y<0)
+        {
+            print_cells(cells, *ant_x, *ant_y, *gen, *current_time, *fps);
+            printf("Ant out of bounds (up)\n");
+            exit(1);
+        } else if(*ant_x<0)
+        {
+            print_cells(cells, *ant_x, *ant_y, *gen, *current_time, *fps);
+            printf("Ant out of bounds (left)\n");
+            exit(1);
+        } else if(*ant_y>=height)
+        {
+            print_cells(cells, *ant_x, *ant_y, *gen, *current_time, *fps);
+            printf("Ant out of bounds (down)\n");
+            exit(1);
+        }
+        
+        *gen+=1;
+    }
+    stop=clock();
+    *current_time=stop-time_start;
+    
+    int sleep_time;
+    if(*fps!=0) sleep_time = 1000 / (*fps) - (stop - start);
+    if (sleep_time > 0 && *fps!=0) Sleep(sleep_time);              //sleep for some time so the fps is more fluid and it doesnt run to quickly in the beginning
 }
 int main(void)
 {
@@ -160,6 +165,7 @@ int main(void)
         get_input(&fps);
         update_ant(cells, &ant_x, &ant_y, &ant_direction, &gen, time_start, &current_time, &fps);
         print_cells(cells, ant_x, ant_y, gen, current_time, fps);
+        if(final==1) exit(0);
     }
     return 0;
 }
