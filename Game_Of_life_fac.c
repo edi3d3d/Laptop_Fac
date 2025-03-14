8a2dcf6a9a3be18f2ea25d6aac56cd11d7c8abdb
edi3d3d <edi3d3d@gmail.com>
2025-03-15 00:49:32 +0200

    working on the rules, i got the idea now

--- Game_Of_life_fac.c
+++ Game_Of_life_fac.c
@@ -1,51 +1,100 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define file_name "D:\\vsc\\Game_Of_Life_Tasks_File.txt"         //introduceti adresa si numele fisierului (exemplu): "D:\\vsc\\Game_Of_Life_Tasks_File.txt"
-typedef struct 
-{
+typedef struct coord{
     int l;
     int c;
-}cell;
-typedef struct binary_tree
-{
+} coord;
+typedef struct binary_tree{
     int depth;
-    cell *value;
-    struct binary_tree* left_value;
-    struct binary_tree* right_value;
-};
-void read_from_file(FILE* data_file, int *size_x, int *size_y, int *nr_iteratii, char **grid)
+    int passed;
+    coord *cell;
+    struct binary_tree* father;
+    struct binary_tree *(son[2]);
+}bTree;
+void read_from_file(FILE* data_file, int *size_l, int *size_c, int *nr_iteratii, bTree tree_start)
 {
-    fscanf(data_file,"%d%d%d", size_y, size_x, nr_iteratii);
-
+    fscanf(data_file,"%d%d%d", size_l, size_c, nr_iteratii);
     fgetc(data_file);
-    (*size_x)++;       //se adauga caracterul \n la finalul fiecarui rand pentru a putea face printarea instanta
     
-    *grid=(char*) malloc((*size_y)*(*size_x));
-
-    for(int j=0; j<(*size_y); j++){
-        fgets((*grid)+j*(*size_x), sizeof(*grid), data_file);
-        (*grid)[(j+1)*(*size_x)]='\n';
-    }
-    (*grid)[(*size_y)*(*size_x)]='\0';        
-    
-}
-void update_cells(FILE* data_file, int size_x, int size_y, int nr_iteratii, char *grid)
-{
-    char *aux_grid=NULL;
-    aux_grid= (char*) malloc (size_y*size_x);
-    int total_size=size_x*size_y;
-    for(int i=1; i<=nr_iteratii; i++)
+    tree_start.cell= (coord*) malloc(sizeof(coord*));
+    tree_start.depth=0;
+    tree_start.father=NULL;
+    tree_start.son[0]=NULL;
+    tree_start.son[1]=NULL;
+    char x;
+    int k=0;
+    for(int i=0; i<(*size_l); i++)
     {
-        
-        for(int j=1; j<=total_size; j++)
+        for(int j=0; j<(*size_c); j++)
         {
-            //update sim on the grid
+            fscanf(data_file, "%c", &x);
             
+             
+            if(x=='X') 
+            {
+                tree_start.cell[k].l=i;
+                tree_start.cell[k].c=j;
+                k++;
+                tree_start.cell= (coord*) realloc(tree_start.cell,sizeof(coord*) * k);
+                printf("%d %d\n", i, j);
+            } 
         }
-        fputs(grid, data_file);
-        fprintf(data_file, "\n");
+        fgetc(data_file);
     }
-    free(aux_grid);
+    tree_start.cell[k].l=0;
+    tree_start.cell[k].c=0;
+}
+void task_cells(FILE* data_file, int size_l, int size_c, int nr_iteratii, bTree *tree_start)
+{
+    bTree *tree_current=tree_start, *next=NULL;
+    char **board= (char**) malloc (size_l);
+    int origin_passes=0, direction=0;           //direction=0 LEFT
+                                                //direction=1 RIGHT
+    for(int i=0;i<size_l; i++)
+    {
+        board[i]= (char*) malloc(size_c);
+        for(int j=0; j<size_c; j++)
+            board[i][j]='+';
+    }
+
+    for(int i=0; (*tree_start).cell[i].c!=0; i++)
+        board
+            [(*tree_start).cell[i].c]
+            [(*tree_start).cell[i].l]
+            ='X';
+    while(origin_passes<3)        //gandeste o conditie, (nr_iteratii *tree)
+    {
+        if((*tree_current).father==NULL) origin_passes++;
+
+
+        if((*tree_current).depth<nr_iteratii)
+        {
+            (*tree_current).son[direction] = (bTree*) malloc(sizeof(bTree));
+            tree_current=(*tree_current).son[direction];
+            direction=(direction+1)%2;
+
+            
+
+        }
+
+
+        /* 
+        
+        if(direction==0) next=(*tree_current).left;
+        if(direction==1) next=(*tree_current).right;
+
+        if(next==NULL)
+        {
+            tree_current=(*tree_current).father;
+            direction=1;
+        } */
+
+    }
+    
+    for(int i=0;i<size_l; i++)
+        free(board[i]);
+    free(board);
 }
 int main()
 {
@@ -56,23 +105,18 @@ int main()
         return 1;
     }
 
-    char *grid=NULL;
+    bTree tree_start;
+
     int nr_tot_task;
     fscanf(data_file, "%d", &nr_tot_task);
 
     for(int i=0; i<nr_tot_task; i++){
 
-        int size_x, size_y, nr_iteratii;
-        read_from_file(data_file, &size_x, &size_y, &nr_iteratii, &grid);
-        
-        update_cells(data_file, size_x, size_y, nr_iteratii, grid);
-
-
-        
+        int size_l, size_c, nr_iteratii;
+        read_from_file(data_file, &size_l, &size_c, &nr_iteratii, tree_start);
+        task_cells(data_file, size_l, size_c, nr_iteratii, &tree_start);
     }
-
-
-    free(grid);
+    
     fclose(data_file);
     return 0;
 }
