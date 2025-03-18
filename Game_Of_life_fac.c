aaecceb5392c9f9d5ddae5b30903c4f2bfbca820
edi3d3d <edi3d3d@gmail.com>
2025-03-18 20:56:46 +0200

    first working code

--- Game_Of_life_fac.c
+++ Game_Of_life_fac.c
@@ -25,12 +25,13 @@ void setup(FILE* data_file, int *size_l, int *size_c, int *nr_iteratii, bTree *t
     (*tree_start).cell= (coord*) malloc(sizeof(coord));
     (*tree_start).son[0]=NULL;
     (*tree_start).son[1]=NULL;
-    char board_value;
+    
     int k=0;
     for(int i=0; i<(*size_l); i++)
     {
         for(int j=0; j<(*size_c); j++)
         {
+            char board_value;
             fscanf(data_file, "%c", &board_value);                  //record the values of X and add the coordonates in the 
             if(board_value=='X')                                    //cell array of each node of the tree/list
             {
@@ -65,7 +66,11 @@ void setup(FILE* data_file, int *size_l, int *size_c, int *nr_iteratii, bTree *t
             int nr_neighbours=0;
             
             for(int k=0; k<8; k++)
-                if(i>=0 && i<size_l && j>=0 && j<size_l) nr_neighbours=((*board)[i + neighbours[k][0]][j + neighbours[k][1]] >> 4);
+                if( ( i + neighbours[k][0] ) >= 0 &&
+                    ( i + neighbours[k][0] ) < (*size_l) &&
+                    ( j + neighbours[k][1] ) >= 0 &&
+                    ( j + neighbours[k][1] ) < (*size_c))
+                    nr_neighbours+=((*board)[i + neighbours[k][0]][j + neighbours[k][1]] >> 4);
             
             (*board)[i][j]+=nr_neighbours;                            //the sum gets added to the cell
         }                                                             //it cannot be bigger then 8 so it uses 4 out of the value's 8 bits
@@ -73,12 +78,12 @@ void setup(FILE* data_file, int *size_l, int *size_c, int *nr_iteratii, bTree *t
 }
 void afis(FILE* data_file, int size_l, int size_c, bTree *tree_node, int task, char **board, int depth)
 {
-    if((task==1 && depth!=0) || task==3)
+    if((task==1 && depth>=0) || task==3)
     {
         for(int i=0; i<size_l; i++)
         {
             for(int j=0; j<size_c; j++)
-                printf("%c ", (board[i][j] >> 4) * ('X' - '+')+'+');
+            printf("%c ", (board[i][j] >> 4) * ('X' - '+')+'+');
             printf("\n");
         }
         printf("\n");
@@ -95,24 +100,10 @@ void afis(FILE* data_file, int size_l, int size_c, bTree *tree_node, int task, c
         exit(0); //hamiltonian, idk
     }
 }
-void node_processing (FILE* data_file, int size_l, int size_c, int nr_iteratii, bTree *tree_node, int task, char **board, int depth, char direction)
+void node_processing (int size_l, int size_c, bTree *tree_node, char **board, char direction)
 {
     int k=0;
-    (*tree_node).cell= (coord*) malloc (sizeof(coord));     
-    
-    for(int i=0; i<size_l; i++)
-    {
-        for(int j=0; j<size_c; j++)
-            printf("%c ", (board[i][j] & 0b00010000));
-        printf("    ");
-
-        for(int j=0; j<size_c; j++)
-            printf("%c ", (board[i][j] & 0b00001111));
-        
-        printf("\n");
-    }
-
-
+    (*tree_node).cell= (coord*) malloc (sizeof(coord));  
     for(int i=0; i<size_l; i++)                     //update node cells pointer of the current node
     {
         for(int j=0; j<size_c; j++)
@@ -163,24 +154,24 @@ void node_processing (FILE* data_file, int size_l, int size_c, int nr_iteratii, 
             { 0, -1},          { 0, +1},
             {+1, -1}, {+1, 0}, {+1, +1}
         };
+        
         int cell_l=(*tree_node).cell[i].l;
         int cell_c=(*tree_node).cell[i].c;
         int state;
-
-        char board_value = board [cell_l] [cell_c];
-        if((board_value & 0b00010000) == 1 ) state = -1;
+        if( ((board[cell_l][cell_c] & 0b00010000) >> 4 ) == 1 ) state = -1;
         else state = 1;
         
-        for(int j=0; j<=8; j++)
-            if(cell_l>=0 && cell_l<size_l && cell_c>=0 && cell_c<size_c) board[ cell_l + neighbours[i][0] ][ cell_c + neighbours[i][1] ] += state;
+        board[cell_l][cell_c]+=state*16; //16 being the 5th bit from right to left
+        
+        for(int j=0; j<8; j++)
+            if( ( cell_l + neighbours[j][0] ) >= 0 &&
+                ( cell_l + neighbours[j][0] ) < size_l &&
+                ( cell_c + neighbours[j][1] ) >= 0 &&
+                ( cell_c + neighbours[j][1] ) < size_c)
+                board[ cell_l + neighbours[j][0] ][ cell_c + neighbours[j][1] ] += state;
+                
+                    
     }
-
-    task_cells(data_file, size_l, size_c, nr_iteratii, tree_node, task, board, depth+1);
-
-    //outdate matrix
-
-    free((*tree_node).cell);
-    free(tree_node);
 }
 void task_cells(FILE* data_file, int size_l, int size_c, int nr_iteratii, bTree *tree_node, int task, char **board, int depth)
 {
@@ -190,11 +181,68 @@ void task_cells(FILE* data_file, int size_l, int size_c, int nr_iteratii, bTree 
     {
         (*tree_node).son[0]= (bTree*) malloc(sizeof(bTree));
         node_processing(size_l, size_c, (*tree_node).son[0], board, 0);
-    }
-    
-    (*tree_node).son[1]= (bTree*) malloc(sizeof(bTree));
-    node_processing(size_l, size_c, (*tree_node).son[1], board, 1);
+        task_cells(data_file, size_l, size_c, nr_iteratii, (*tree_node).son[0], task, board, depth+1);
+        //deupdate matrix
+        for(int i=0; (*((*tree_node).son[0])).cell[i].c != -1; i++)
+        {
+            int neighbours[8][2]={
+                {-1, -1}, {-1, 0}, {-1, +1},
+                { 0, -1},          { 0, +1},
+                {+1, -1}, {+1, 0}, {+1, +1}
+            };
+            int cell_l=(*((*tree_node).son[0])).cell[i].l;
+            int cell_c=(*((*tree_node).son[0])).cell[i].c;
+            int state;
+            if( ((board [cell_l] [cell_c] & 0b00010000) >> 4 ) == 1 ) state = -1;
+            else state = 1;
 
+            board[cell_l][cell_c]+=state*16; //16 being the 5th bit from right to left
+        
+            for(int j=0; j<8; j++)
+                if(
+                    cell_l>=0 &&
+                    cell_l<size_l &&
+                    cell_c>=0 &&
+                    cell_c<size_c)
+                    board[ cell_l + neighbours[i][0] ][ cell_c + neighbours[i][1] ] += state;
+        }
+
+        free((*(*tree_node).son[0]).cell);
+        free((*tree_node).son[0]);
+    }
+
+    (*tree_node).son[1]= (bTree*) malloc(sizeof(bTree));
+        node_processing(size_l, size_c, (*tree_node).son[1], board, 1);
+        task_cells(data_file, size_l, size_c, nr_iteratii, (*tree_node).son[1], task, board, depth+1);
+        //deupdate matrix
+        for(int i=0; (*((*tree_node).son[1])).cell[i].c != -1; i++)
+        {
+            int neighbours[8][2]={
+                {-1, -1}, {-1, 0}, {-1, +1},
+                { 0, -1},          { 0, +1},
+                {+1, -1}, {+1, 0}, {+1, +1}
+            };
+            int cell_l=(*((*tree_node).son[1])).cell[i].l;
+            int cell_c=(*((*tree_node).son[1])).cell[i].c;
+            int state;
+            if( ((board [cell_l] [cell_c] & 0b00010000) >> 4 ) == 1 ) state = -1;
+            else state = 1;
+
+            board[cell_l][cell_c]+=state*16; //16 being the 5th bit from right to left
+        
+            for(int j=0; j<8; j++)
+                if(
+                    cell_l>=0 &&
+                    cell_l<size_l &&
+                    cell_c>=0 &&
+                    cell_c<size_c)
+                    board[ cell_l + neighbours[i][0] ][ cell_c + neighbours[i][1] ] += state;
+        }
+
+        free((*(*tree_node).son[1]).cell);
+        free((*tree_node).son[1]);
+    
+    free(tree_node);
 }
 int main()
 {
